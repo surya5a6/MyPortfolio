6b5b4aaf7087e7228c2dd2cb8fd8619718cff885
tejas1 <suryateja.malisetti@pega.com>
2021-05-24 18:43:50 +0530

    added comit

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -14,7 +14,7 @@ const Blogs = (props) => {
         <div className="underline"></div>
       </div>
       <div className="section-center blog-center">
-        <div className="card">
+        {/* <div className="card">
           <div className="card-side card-front">
             <img src={project1} alt=""></img>
             <div className="card-info">
@@ -29,8 +29,8 @@ const Blogs = (props) => {
           <div className="card-side card-back">
             <button className="btn">read more</button>
           </div>
-        </div>
-        <div className="card">
+        </div> */}
+        {/* <div className="card">
           <div className="card-side card-front">
             <img src={project2} alt=""></img>
             <div className="card-info">
@@ -45,8 +45,8 @@ const Blogs = (props) => {
           <div className="card-side card-back">
             <button className="btn">read more</button>
           </div>
-        </div>
-        <div className="card">
+        </div> */}
+        {/* <div className="card">
           <div className="card-side card-front">
             <img src={project3} alt=""></img>
             <div className="card-info">
@@ -61,21 +61,21 @@ const Blogs = (props) => {
           <div className="card-side card-back">
             <button className="btn">read more</button>
           </div>
-        </div>
+        </div> */}
         <div className="card">
           <div className="card-side card-front">
             <img src={project4} alt=""></img>
             <div className="card-info">
-              <h4>article title</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione illum nesciunt, autem accusantium debitis in odio adipisci! Amet, totam numquam?</p>
+              <h4>Frequency Counter Pattern</h4>
+              <p>Frequency counter pattern uses objects or sets to collect values or frequencies of values. By following this approach we can avoid the time complexity of O(n²)...</p>
               <div className="card-footer">
                 <img src={myPic} alt=""/>
-                <p>7 min read</p>
+                <p>3 min read</p>
               </div>
             </div>
           </div>
           <div className="card-side card-back">
-            <button className="btn">read more</button>
+            <a href="https://suryatejaamr.medium.com/frequency-counter-pattern-in-javascript-754870f95fb8" target="_blank" className="btn" rel="noreferrer">read more</a>
           </div>
         </div>
       </div>
